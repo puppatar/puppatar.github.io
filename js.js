3b6bc200257cc9bcd8e697d36ccea68722279631
puppatar <niwang66@gmail.com>
2019-03-04 17:58:10 -0800

    finish

---
+++ js.js
@@ -0,0 +1,10 @@
+let name = window.prompt("type a name")
+console.log(name)
+document.getElementById('nick').innerText = name;
+let object = window.prompt("type a object")
+document.getElementById('object').innerText = object;
+let verb = window.prompt("type a verb")
+document.getElementById('verb').innerText = verb;
+let verb1 = window.prompt("type a verb")
+document.getElementById('verb1').innerText = verb1;
+
