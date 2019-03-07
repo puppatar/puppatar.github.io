287729eb04f9596cd9fd2c9c4ad5657fe5bf8a8f
puppatar <niwang66@gmail.com>
2019-03-06 17:56:10 -0800

    might add more

--- js.js
+++ js.js
@@ -1,10 +1,31 @@
-let name = window.prompt("type a name")
-console.log(name)
-document.getElementById('nick').innerText = name;
-let object = window.prompt("type a object")
-document.getElementById('object').innerText = object;
-let verb = window.prompt("type a verb")
+
+
+function buttonName() {
+    let name = window.prompt("type a name");
+    document.getElementById('nick').innerText = name;
+    console.log(name);
+}
+function object() {
+    let object = window.prompt("type a handheld object");
+    document.getElementById('object').innerText = object;
+    }
+function verb3 () {
+    let verb3 = window.prompt('type a past tense verb');
+    document.getElementById('verb').innerText = verb3 ;
+        }
+
+    
+
+let verb = window.prompt("type a verb");
 document.getElementById('verb').innerText = verb;
-let verb1 = window.prompt("type a verb")
+let verb1 = window.prompt("type a present verb");
 document.getElementById('verb1').innerText = verb1;
+let word = window.prompt("past tense verb");
+document.getElementById('word').innerText = word;
+
+document.getElementById('nick1').onclick = buttonName;
+document.getElementById('handheld').onclick = object;
+document.getElementById('verbs').onclick = verb3;
+
+
 
