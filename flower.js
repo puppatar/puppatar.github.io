9c10b61d7ec2274d3e8f62042fd7feb62f88b87c
puppatar <niwang66@gmail.com>
2019-03-18 18:01:25 -0700

    make a copy of flower

--- flower.js
+++ flower.js
@@ -1,3 +1,36 @@
-var draw = SVG('flower').size(300, 130)
 
-var rect = draw.circle(100).fill('#f06').move(20, 20)
+var draw = SVG('flowers').size(500, 500)
+
+
+
+var rect = draw.rect(100, 100).size(10,70).fill('green').move(115, 250)
+
+var petal;
+petal2(130, 150);
+petal2(120,100);
+petal2(65, 70);
+petal2(30,120);
+petal2(20,160);
+// make more petals
+
+function petal2(x,y){
+     petal = draw.circle(100,80 ).fill('yellow').move(x,y);
+
+}
+
+function flower(x,y){
+    var circle = draw.circle(150, 100).fill('green').move(x, y)  
+    var petal = draw.circle(100,80 ).fill('yellow').move(x - 20,y -  20);
+
+}
+
+flower(300, 300)
+flower(200, 200)
+
+
+
+
+
+
+
+
