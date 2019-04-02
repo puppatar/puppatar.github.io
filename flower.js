ad051f953341124e50ce48c324be889296ea951c
puppatar <niwang66@gmail.com>
2019-04-01 17:59:26 -0700

    f

--- flower.js
+++ flower.js
@@ -5,15 +5,6 @@ var draw = SVG('flowers').size(2000, 2000)
 
 
 
-// var rect = draw.rect(100, 100).size(10,70).fill('green').move(115, 250)
-
-// var petal;
-// petal2(130, 150);
-// petal2(120,100);
-// petal2(65, 70);
-// petal2(30,120);
-// petal2(20,160);
-// make more petals
 
 function petal2(x,y){
      petal = draw.circle(100,80 ).fill('yellow').move(x,y);
---
+++ p.js
@@ -0,0 +1,23 @@
+const canvaswidth = 700;
+const canvasheight = 500;
+const blockwidth = 75;
+const blockheight = 5;
+let numrow = 10;
+const blocknumb = 5;
+
+
+let draw = SVG('block').size(canvaswidth, canvasheight)
+
+const White = '#fff';
+const Black = '#000';
+
+for(let i = 0; i < numrow; i++){
+    numrow = i * blockheight;
+
+    for(let j = 0; j < numrow; j++){
+        blocknumb = j * blockwidth;
+
+        var rect = draw.rect(canvasheight,canvaswidth).fill('white')
+        }
+        
+    }
