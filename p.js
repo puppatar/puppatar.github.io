056b6c647f5a99bb1d3cc7b40fdd4f563faf3f19
puppatar <niwang66@gmail.com>
2019-04-03 16:16:14 -0700

    bb

--- p.js
+++ p.js
@@ -3,7 +3,7 @@ const canvasheight = 500;
 const blockwidth = 75;
 const blockheight = 5;
 let numrow = 10;
-const blocknumb = 5;
+let blocknumb = 5;
 
 
 let draw = SVG('block').size(canvaswidth, canvasheight)
@@ -12,10 +12,10 @@ const White = '#fff';
 const Black = '#000';
 
 for(let i = 0; i < numrow; i++){
-    numrow = i * blockheight;
+    numrow = i + blockheight;
 
     for(let j = 0; j < numrow; j++){
-        blocknumb = j * blockwidth;
+        blocknumb = j + blockwidth;
 
         var rect = draw.rect(canvasheight,canvaswidth).fill('white')
         }
